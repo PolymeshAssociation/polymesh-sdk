eecf656b91d29f16cf368049603f460d316e11a9
Victor Wiebe <victor.wiebe@polymath.network>
2020-01-24 14:03:40 -0800

    fix: review comments

--- release.config.js
+++ release.config.js
@@ -2,8 +2,6 @@ module.exports = {
   repositoryUrl: "https://github.com/PolymathNetwork/polymesh-sdk.git",
   branches: [
     "master",
-    "next",
-    "next-major",
     {
       name: "beta",
       prerelease: true
