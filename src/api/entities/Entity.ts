59c05af65622723f46dd6287a44c6121abd883f5
Shuffledex <shuffledex@gmail.com>
2020-02-07 08:14:01 -0300

    feat: entity class

---
+++ src/api/entities/Entity.ts
@@ -0,0 +1,6 @@
+/**
+ * Represents an object or resource in the Polymesh Ecosystem with its own set of properties and functionality
+ */
+export abstract class Entity {
+  public abstract readonly uuid: string;
+}
