d1542f9189aa9cfa6e65a97bf28c244e900120f4
Shuffledex <shuffledex@gmail.com>
2020-02-11 11:21:04 -0300

    feat: polymesh class

---
+++ src/Polymesh.ts
@@ -0,0 +1,42 @@
+import { ApiPromise, WsProvider } from '@polymathnetwork/polkadot/api';
+import { Context } from './Context';
+
+interface ConnectParams {
+  node: string;
+  seed?: string;
+}
+
+interface Connect {
+  (params: ConnectParams): Promise<Polymesh>;
+}
+
+/**
+ * Main entry point of the Polymash SDK
+ */
+export class Polymesh {
+  public isConnected = false;
+  private context: Context = {} as Context;
+
+  public connect: Connect = async ({
+    node,
+    seed = undefined,
+  }: ConnectParams): Promise<Polymesh> => {
+    let polymeshApi: ApiPromise;
+    try {
+      polymeshApi = await ApiPromise.create({
+        provider: new WsProvider(node),
+      });
+    } catch (e) {
+      throw new Error('Connection error');
+    }
+
+    this.isConnected = true;
+
+    this.context = await Context.create({
+      polymeshApi,
+      seed,
+    });
+
+    return this;
+  };
+}
