29ac1c0768d0f16d32866ebc0a7751a9e1e4ac42
Shuffledex <shuffledex@gmail.com>
2020-04-14 08:26:39 -0300

    fix: add some validations

--- src/api/procedures/setIssuanceData.ts
+++ src/api/procedures/setIssuanceData.ts
@@ -1,7 +1,7 @@
 import { SecurityToken } from '~/api/entities';
 import { PolymeshError, Procedure } from '~/base';
 import { ErrorCode, IssuanceData, Role, RoleType } from '~/types';
-import { stringToTicker } from '~/utils';
+import { numberToBalance, stringToIdentityId, stringToTicker } from '~/utils';
 
 export interface SetIssuanceDataParams {
   issuances: IssuanceData[];
@@ -20,13 +20,34 @@ export async function prepareSetIssuanceData(
 ): Promise<SecurityToken> {
   const {
     context: {
-      polymeshApi: { query, tx },
+      polymeshApi: {
+        query,
+        tx: { asset },
+      },
     },
     context,
   } = this;
   const { ticker, issuances } = args;
 
+  const investors = issuances.map(issuance => stringToIdentityId(issuance.did, context));
+  const values = issuances.map(issuance => numberToBalance(issuance.balance, context));
+
+  if (investors.length !== values.length) {
+    throw new PolymeshError({
+      code: ErrorCode.ValidationError,
+      message: 'You must set the same number of investors as values',
+    });
+  }
+
+  const securityToken = new SecurityToken({ ticker }, context);
+  const { isDivisible, totalSupply } = await securityToken.details();
+  // check granularity
+
+  debugger;
+
   const rawTicker = stringToTicker(ticker, context);
+
+  // this.addTransaction(asset.batchIssue, {}, rawTicker, [], []);
 
   return new SecurityToken({ ticker }, context);
 }
