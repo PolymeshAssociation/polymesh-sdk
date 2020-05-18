6047b00c54fb3da2832206b3ee9f4f83bf37f8a8
Jeremias Diaz <monitz87@gmail.com>
2020-05-18 19:29:51 -0400

    feat: add missing sub and comment out unused method

--- src/Polymesh.ts
+++ src/Polymesh.ts
@@ -120,7 +120,7 @@ export class Polymesh {
     return transferPolyX.prepare(args, this.context);
   }
 
-  // TODO: uncomment the method after v1
+  // TODO: uncomment for v2
   /**
    * Get the POLYX balance of the current account
    * NOTE: We don't expose this method for Testnet v1
