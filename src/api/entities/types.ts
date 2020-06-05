c60feda2b220b6a3faacaab0399d948b826d1c61
Jeremias Diaz <monitz87@gmail.com>
2020-06-05 14:06:23 -0400

    feat: export entities as types

--- src/api/entities/types.ts
+++ src/api/entities/types.ts
@@ -1,2 +1,14 @@
+import {
+  AuthorizationRequest as AuthorizationRequestClass,
+  Identity as IdentityClass,
+  SecurityToken as SecurityTokenClass,
+  TickerReservation as TickerReservationClass,
+} from './';
+
+export type SecurityToken = InstanceType<typeof SecurityTokenClass>;
+export type TickerReservation = InstanceType<typeof TickerReservationClass>;
+export type AuthorizationRequest = InstanceType<typeof AuthorizationRequestClass>;
+export type Identity = InstanceType<typeof IdentityClass>;
+
 export * from './TickerReservation/types';
 export * from './SecurityToken/types';
