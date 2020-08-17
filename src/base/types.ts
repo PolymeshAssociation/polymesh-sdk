e07bdd8bd1e512e90e413072682def884412996f
Jeremias Diaz <monitz87@gmail.com>
2020-08-17 18:01:55 -0400

    feat: add PolymeshError typeguard

--- src/base/types.ts
+++ src/base/types.ts
@@ -1,5 +1,16 @@
+import { PolymeshError as PolymeshErrorClass } from './PolymeshError';
 import { PolymeshTransaction as PolymeshTransactionClass } from './PolymeshTransaction';
 import { TransactionQueue as TransactionQueueClass } from './TransactionQueue';
 
 export type PolymeshTransaction = InstanceType<typeof PolymeshTransactionClass>;
 export type TransactionQueue = InstanceType<typeof TransactionQueueClass>;
+export type PolymeshError = InstanceType<typeof PolymeshErrorClass>;
+
+/**
+ * @hidden
+ */
+export function isPolymeshError(err: unknown): err is PolymeshError {
+  const error = err as PolymeshError;
+
+  return typeof error.code === 'string' && typeof error.message === 'string';
+}
