070148318eb6fc926646d733b6ec6030e5ac490a
Jeremias Diaz <monitz87@gmail.com>
2020-08-17 22:19:52 -0400

    test: test PolymeshError typeguard

--- src/base/types.ts
+++ src/base/types.ts
@@ -5,12 +5,4 @@ import { TransactionQueue as TransactionQueueClass } from './TransactionQueue';
 export type PolymeshTransaction = InstanceType<typeof PolymeshTransactionClass>;
 export type TransactionQueue = InstanceType<typeof TransactionQueueClass>;
 export type PolymeshError = InstanceType<typeof PolymeshErrorClass>;
-
-/**
- * @hidden
- */
-export function isPolymeshError(err: unknown): err is PolymeshError {
-  const error = err as PolymeshError;
-
-  return typeof error.code === 'string' && typeof error.message === 'string';
-}
+export { isPolymeshError } from './PolymeshError';
