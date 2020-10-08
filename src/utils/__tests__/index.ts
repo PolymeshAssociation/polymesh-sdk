8f4aa1e20070e6e1a6717cfc183552721c85727a
Jeremias Diaz <monitz87@gmail.com>
2020-10-08 11:09:49 -0300

    fix: convert claims correctly

--- src/utils/__tests__/index.ts
+++ src/utils/__tests__/index.ts
@@ -1713,10 +1713,13 @@ describe('claimToMeshClaim and meshClaimToClaim', () => {
       scope: { type: ScopeType.Identity, value: 'someTickerDid' },
     };
     const fakeResult = ('meshClaim' as unknown) as MeshClaim;
+    const fakeScope = ('scope' as unknown) as MeshScope;
 
-    dsMockUtils
-      .getCreateTypeStub()
-      .withArgs('Claim', { [value.type]: [value.code, value.scope] })
+    const createTypeStub = dsMockUtils.getCreateTypeStub();
+
+    createTypeStub.withArgs('Scope', sinon.match.any).returns(fakeScope);
+    createTypeStub
+      .withArgs('Claim', { [value.type]: [value.code, scopeToMeshScope(value.scope, context)] })
       .returns(fakeResult);
 
     let result = claimToMeshClaim(value, context);
@@ -1728,9 +1731,8 @@ describe('claimToMeshClaim and meshClaimToClaim', () => {
       scope: { type: ScopeType.Identity, value: 'someTickerDid' },
     };
 
-    dsMockUtils
-      .getCreateTypeStub()
-      .withArgs('Claim', { [value.type]: value.scope })
+    createTypeStub
+      .withArgs('Claim', { [value.type]: scopeToMeshScope(value.scope, context) })
       .returns(fakeResult);
 
     result = claimToMeshClaim(value, context);
@@ -1741,10 +1743,7 @@ describe('claimToMeshClaim and meshClaimToClaim', () => {
       type: ClaimType.NoData,
     };
 
-    dsMockUtils
-      .getCreateTypeStub()
-      .withArgs('Claim', { [value.type]: null })
-      .returns(fakeResult);
+    createTypeStub.withArgs('Claim', { [value.type]: null }).returns(fakeResult);
 
     result = claimToMeshClaim(value, context);
 
