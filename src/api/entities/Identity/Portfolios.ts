869e0a10622f47974276c3954ec69e2e66cdb786
Shuffledex <shuffledex@gmail.com>
2020-10-20 10:51:50 -0300

    chore: use this.context

--- src/api/entities/Identity/Portfolios.ts
+++ src/api/entities/Identity/Portfolios.ts
@@ -10,7 +10,6 @@ export class Portfolios extends Namespace<Identity> {
    * Create a new Portfolio for the Current Identity
    */
   public createPortfolio(args: { name: string }): Promise<TransactionQueue<NumberedPortfolio>> {
-    const { context } = this;
-    return createPortfolio.prepare(args, context);
+    return createPortfolio.prepare(args, this.context);
   }
 }
