45100e5ab94d8c45760fdbe0bc55e24843d8935e
Federico Elgarte <shuffledex@gmail.com>
2021-02-02 17:26:11 -0300

    Update src/api/procedures/modifyClaims.ts
    Co-authored-by: Jeremías Díaz <monitz87@gmail.com>

--- src/api/procedures/modifyClaims.ts
+++ src/api/procedures/modifyClaims.ts
@@ -248,7 +248,7 @@ export async function prepareModifyClaims(
             code: ErrorCode.ValidationError,
             message: 'A target Identity cannot have CDD claims with different IDs',
             data: {
-              invalidCddClaims: invalidCddClaims,
+              invalidCddClaims,
             },
           });
         }
