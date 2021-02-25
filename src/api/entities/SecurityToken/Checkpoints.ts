c420722600eef49108c18de5e75ea9219fb394c5
Jeremias Diaz <monitz87@gmail.com>
2021-02-25 13:19:40 -0300

    docs(checkpoints): clarify need for filtering checkpoints by createdAt

--- src/api/entities/SecurityToken/Checkpoints.ts
+++ src/api/entities/SecurityToken/Checkpoints.ts
@@ -53,6 +53,7 @@ export class Checkpoints extends Namespace<SecurityToken> {
         checkpoint: new Checkpoint({ id: u64ToBigNumber(id), ticker }, context),
         createdAt: momentToDate(timestamp),
       }))
+      // the query also returns the next scheduled checkpoint (which hasn't been created yet)
       .filter(({ createdAt }) => createdAt <= now);
 
     return {
