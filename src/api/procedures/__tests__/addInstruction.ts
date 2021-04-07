14f6242bc624f198deeda1207c69928b1f5491f1
Victor <victor.g.vicente@gmail.com>
2021-04-06 21:44:14 -0300

    test: add missing properties to mocked balances

--- src/api/procedures/__tests__/addInstruction.ts
+++ src/api/procedures/__tests__/addInstruction.ts
@@ -85,7 +85,9 @@ describe('addInstruction procedure', () => {
 
   beforeAll(() => {
     dsMockUtils.initMocks({
-      contextOptions: { balance: { free: new BigNumber(500), locked: new BigNumber(0) } },
+      contextOptions: {
+        balance: { free: new BigNumber(500), locked: new BigNumber(0), total: new BigNumber(500) },
+      },
     });
     procedureMockUtils.initMocks();
     entityMockUtils.initMocks();
