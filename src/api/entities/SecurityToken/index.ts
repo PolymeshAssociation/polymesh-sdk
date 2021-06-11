1057de2d78b8268b2d2cf6531456a15afbfd37a7
Jeremias Diaz <monitz87@gmail.com>
2021-06-11 01:13:34 -0400

    feat(external agents): create external agent base code
    Also modified `DefaultTrustedClaimIssuer`
    
    BREAKING CHANGE: `DefaultTrustedClaimIssuer` now extends `Identity`. This means its `identity:
    Identity` property was removed. Since the class is now an extension of `Identity`, the did can be
    obtained via the `did` property

--- src/api/entities/SecurityToken/index.ts
+++ src/api/entities/SecurityToken/index.ts
@@ -47,6 +47,7 @@ import { Checkpoints } from './Checkpoints';
 import { Compliance } from './Compliance';
 import { CorporateActions } from './CorporateActions';
 import { Documents } from './Documents';
+import { ExternalAgents } from './ExternalAgents';
 import { Issuance } from './Issuance';
 import { Offerings } from './Offerings';
 import { Settlements } from './Settlements';
@@ -98,6 +99,7 @@ export class SecurityToken extends Entity<UniqueIdentifiers> {
   public offerings: Offerings;
   public checkpoints: Checkpoints;
   public corporateActions: CorporateActions;
+  public externalAgents: ExternalAgents;
 
   /**
    * @hidden
@@ -119,6 +121,7 @@ export class SecurityToken extends Entity<UniqueIdentifiers> {
     this.offerings = new Offerings(this, context);
     this.checkpoints = new Checkpoints(this, context);
     this.corporateActions = new CorporateActions(this, context);
+    this.externalAgents = new ExternalAgents(this, context);
 
     this.transferOwnership = createProcedureMethod(
       { getProcedureAndArgs: args => [transferTokenOwnership, { ticker, ...args }] },
