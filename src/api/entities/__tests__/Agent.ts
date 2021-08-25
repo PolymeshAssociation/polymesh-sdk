cefacba13fc026d543e0ec8e453b03227f1db588
Shuffledex <shuffledex@gmail.com>
2021-08-25 11:57:03 -0300

    feat: addedAt method

--- src/api/entities/__tests__/Agent.ts
+++ src/api/entities/__tests__/Agent.ts
@@ -1,10 +1,15 @@
+import BigNumber from 'bignumber.js';
 import sinon from 'sinon';
 
 import { KnownPermissionGroup } from '~/api/entities/KnownPermissionGroup';
 import { Agent, Context, Identity } from '~/internal';
+import { eventByIndexedArgs } from '~/middleware/queries';
+import { EventIdEnum, ModuleIdEnum } from '~/middleware/types';
 import { dsMockUtils, entityMockUtils } from '~/testUtils/mocks';
 import { PermissionGroupType } from '~/types';
+import { MAX_TICKER_LENGTH } from '~/utils/constants';
 import * as utilsConversionModule from '~/utils/conversion';
+import * as utilsInternalModule from '~/utils/internal';
 
 describe('Agent class', () => {
   const did = 'someDid';
@@ -86,6 +91,48 @@ describe('Agent class', () => {
 
       expect(result instanceof KnownPermissionGroup).toEqual(true);
       expect((result as KnownPermissionGroup).type).toEqual(PermissionGroupType.Full);
+    });
+  });
+
+  describe('method: addedAt', () => {
+    test('should return the event identifier object of the agent added', async () => {
+      const blockNumber = new BigNumber(1234);
+      const blockDate = new Date('4/14/2020');
+      const eventIdx = 1;
+      const variables = {
+        moduleId: ModuleIdEnum.Externalagents,
+        eventId: EventIdEnum.AgentAdded,
+        eventArg1: utilsInternalModule.padString(ticker, MAX_TICKER_LENGTH),
+      };
+      const fakeResult = { blockNumber, blockDate, eventIndex: eventIdx };
+      const agent = new Agent({ did, ticker }, context);
+
+      dsMockUtils.createApolloQueryStub(eventByIndexedArgs(variables), {
+        /* eslint-disable @typescript-eslint/naming-convention */
+        eventByIndexedArgs: {
+          block_id: blockNumber.toNumber(),
+          block: { datetime: blockDate },
+          event_idx: eventIdx,
+        },
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      const result = await agent.addedAt();
+
+      expect(result).toEqual(fakeResult);
+    });
+
+    test('should return null if the query result is empty', async () => {
+      const variables = {
+        moduleId: ModuleIdEnum.Externalagents,
+        eventId: EventIdEnum.AgentAdded,
+        eventArg1: utilsInternalModule.padString(ticker, MAX_TICKER_LENGTH),
+      };
+      const agent = new Agent({ did, ticker }, context);
+
+      dsMockUtils.createApolloQueryStub(eventByIndexedArgs(variables), {});
+      const result = await agent.addedAt();
+      expect(result).toBeNull();
     });
   });
 });
