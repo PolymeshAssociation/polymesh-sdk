3b99404968c7ec817e37c97dffa213afe16af341
Jeremias Diaz <monitz87@gmail.com>
2021-09-29 14:35:36 -0300

    feat: 🎸 force code duplication

--- src/sonarTest.ts
+++ src/sonarTest.ts
@@ -1,146 +1,177 @@
 /* eslint-disable require-jsdoc */
-const myArray = [1, '2', null, 4, 5, 6, 'siete', null, 9, 10, 'eleven', null];
+const myArray: (string | number | null)[] = [
+  1,
+  '2',
+  null,
+  4,
+  5,
+  6,
+  'siete',
+  null,
+  9,
+  10,
+  'eleven',
+  null,
+];
 
-export function element0(): string | number | null {
+export function firstElement(): string | number | null {
   const element = myArray[0];
 
   return element;
 }
 
-export function element1(): string | number | null {
-  const element = myArray[1];
-
-  return element;
+export function secondElement(): string | number | null {
+  return myArray[1];
 }
 
-export function element2(): string | number | null {
-  const element = myArray[2];
-
-  return element;
+export function thirdElement(): string | number | null {
+  return myArray[2];
 }
 
-export function element3(): string | number | null {
-  const element = myArray[3];
+export function someConvolutedPieceOfCode(array: number[]): number[] {
+  array.reverse();
+  array.sort();
+  const otherArray = array.map(n => n * 2);
 
-  return element;
+  otherArray.reverse();
+  otherArray.sort();
+
+  const thirdArray = array.concat(otherArray);
+
+  thirdArray.reverse();
+  thirdArray.sort();
+  thirdArray.reverse();
+  thirdArray.sort();
+  thirdArray.reverse();
+  thirdArray.sort();
+  thirdArray.reverse();
+  thirdArray.sort();
+  thirdArray.reverse();
+  thirdArray.sort();
+
+  const fourthArray = thirdArray.map(n => n - 1);
+  fourthArray.reverse();
+  fourthArray.sort();
+  fourthArray.reverse();
+  fourthArray.sort();
+  fourthArray.reverse();
+  fourthArray.sort();
+  fourthArray.reverse();
+  fourthArray.sort();
+  fourthArray.reverse();
+  fourthArray.sort();
+
+  if (fourthArray.length >= 10) {
+    fourthArray.splice(1, 5);
+  }
+
+  switch (fourthArray[0]) {
+    case 0: {
+      console.log('ZERO');
+
+      break;
+    }
+    case 1: {
+      console.log('ONE');
+
+      break;
+    }
+    case 2: {
+      console.log('TWO');
+
+      break;
+    }
+    case 3: {
+      console.log('THREE');
+
+      break;
+    }
+    case 4: {
+      console.log('FOUR');
+
+      break;
+    }
+    default: {
+      console.log('NONE');
+
+      break;
+    }
+  }
+
+  return fourthArray;
 }
 
-export function element4(): string | number | null {
-  const element = myArray[4];
+export function anotherConvolutedPieceOfCodeThatDoesSomethingVerySimilar(): number[] {
+  const someArray = [1, 2, 3, 4];
 
-  return element;
-}
+  someArray.reverse();
+  someArray.sort();
+  const secondArray = someArray.map(n => n * 2);
 
-export function element5(): string | number | null {
-  const element = myArray[5];
+  secondArray.reverse();
+  secondArray.sort();
 
-  return element;
-}
+  const renamedArray = someArray.concat(secondArray);
 
-export function element6(): string | number | null {
-  const element = myArray[6];
+  renamedArray.reverse();
+  renamedArray.sort();
+  renamedArray.reverse();
+  renamedArray.sort();
+  renamedArray.reverse();
+  renamedArray.sort();
+  renamedArray.reverse();
+  renamedArray.sort();
+  renamedArray.reverse();
+  renamedArray.sort();
 
-  return element;
-}
+  const notTheFourth = renamedArray.map(n => n - 1);
+  notTheFourth.reverse();
+  notTheFourth.sort();
+  notTheFourth.reverse();
+  notTheFourth.sort();
+  notTheFourth.reverse();
+  notTheFourth.sort();
+  notTheFourth.reverse();
+  notTheFourth.sort();
+  notTheFourth.reverse();
+  notTheFourth.sort();
 
-export function element7(): string | number | null {
-  const element = myArray[7];
+  if (notTheFourth.length >= 10) {
+    notTheFourth.splice(1, 5);
+  }
 
-  return element;
-}
+  switch (notTheFourth[0]) {
+    case 0: {
+      console.log('ZERO');
 
-export function element8(): string | number | null {
-  const element = myArray[8];
+      break;
+    }
+    case 1: {
+      console.log('ONE');
 
-  return element;
-}
+      break;
+    }
+    case 2: {
+      console.log('TWO');
 
-export function element9(): string | number | null {
-  const element = myArray[9];
+      break;
+    }
+    case 3: {
+      console.log('THREE');
 
-  return element;
-}
+      break;
+    }
+    case 4: {
+      console.log('FOUR');
 
-export function element10(): string | number | null {
-  const element = myArray[10];
+      break;
+    }
+    default: {
+      console.log('NONE');
 
-  return element;
-}
+      break;
+    }
+  }
 
-export function element11(): string | number | null {
-  const element = myArray[11];
-
-  return element;
-}
-
-export function element12(): string | number | null {
-  const element = myArray[12];
-
-  return element;
-}
-
-export function element13(): string | number | null {
-  const element = myArray[13];
-
-  return element;
-}
-
-export function element14(): string | number | null {
-  const element = myArray[14];
-
-  return element;
-}
-
-export function element15(): string | number | null {
-  const element = myArray[15];
-
-  return element;
-}
-
-export function element16(): string | number | null {
-  const element = myArray[16];
-
-  return element;
-}
-
-export function element17(): string | number | null {
-  const element = myArray[17];
-
-  return element;
-}
-
-export function element18(): string | number | null {
-  const element = myArray[18];
-
-  return element;
-}
-
-export function element19(): string | number | null {
-  const element = myArray[19];
-
-  return element;
-}
-
-export function element20(): string | number | null {
-  const element = myArray[20];
-
-  return element;
-}
-
-export function element21(): string | number | null {
-  const element = myArray[21];
-
-  return element;
-}
-
-export function element22(): string | number | null {
-  const element = myArray[22];
-
-  return element;
-}
-
-export function element23(): string | number | null {
-  const element = myArray[23];
-
-  return element;
+  return notTheFourth;
 }
