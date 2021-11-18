d121d9d077953b572b69791253c300c2f361f0e3
Federico Elgarte <shuffledex@gmail.com>
2021-11-18 17:21:35 -0300

    Update src/api/procedures/__tests__/removeAssetRequirement.ts
    Co-authored-by: Jeremías Díaz <monitz87@gmail.com>

--- src/api/procedures/__tests__/removeAssetRequirement.ts
+++ src/api/procedures/__tests__/removeAssetRequirement.ts
@@ -114,7 +114,7 @@ describe('removeAssetRequirement procedure', () => {
     ).rejects.toThrow('The supplied id is not valid');
   });
 
-  test('should add a remove asset compliance transaction to the queue', async () => {
+  test('should add a remove compliance requirement transaction to the queue', async () => {
     const rawId = dsMockUtils.createMockU32(id.toNumber());
     sinon.stub(utilsConversionModule, 'numberToU32').returns(rawId);
 
