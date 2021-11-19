afb75c0d0cf556aa408fee577c4a0092c6c06352
Shuffledex <shuffledex@gmail.com>
2021-11-19 10:02:57 -0300

    test: 💍 fix

--- src/api/entities/SecurityToken/Compliance/__tests__/Requirements.ts
+++ src/api/entities/SecurityToken/Compliance/__tests__/Requirements.ts
@@ -109,7 +109,7 @@ describe('Requirements class', () => {
       const requirements = new Requirements(token, context);
 
       const args = {
-        requirements: [
+        conditions: [
           {
             type: ConditionType.IsPresent,
             claim: {
