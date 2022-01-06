b05111210fe21e9cba68cc5fa329d10ec5a62e7f
Eric <ericrichardson@polymath.network>
2022-01-06 09:34:01 -0500

    refactor: 💡 Group method declaration together

--- src/Assets.ts
+++ src/Assets.ts
@@ -69,6 +69,14 @@ export class Assets {
   public claimClassicTicker: ProcedureMethod<ClaimClassicTickerParams, TickerReservation>;
 
   /**
+   * Create a Security Token
+   *
+   * @note if ticker is already reserved, then required role:
+   *   - Ticker Owner
+   */
+  public createToken: ProcedureMethod<CreateSecurityTokenWithTickerParams, SecurityToken>;
+
+  /**
    * Check if a ticker hasn't been reserved
    *
    * @note can be subscribed to
@@ -223,12 +231,4 @@ export class Assets {
 
     return token;
   }
-
-  /**
-   * Create a Security Token
-   *
-   * @note if ticker is already reserved, then required role:
-   *   - Ticker Owner
-   */
-  public createToken: ProcedureMethod<CreateSecurityTokenWithTickerParams, SecurityToken>;
 }
