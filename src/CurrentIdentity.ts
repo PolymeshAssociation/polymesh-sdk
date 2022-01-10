5e1fab36541c4bf862e60978e057b78b034e4f84
polymath-eric <86971143+polymath-eric@users.noreply.github.com>
2022-01-10 17:25:24 -0500

    feat: 🎸 Move createVenue into settlements namespace (#656)
    * feat: 🎸 Move createVenue into settlements namespace
    
    sdk.currentIdentity.createVenue -> sdk.settlements.createVenue
    
    BREAKING CHANGE: 🧨 createVenue moved from currentIdentity to settlements

--- src/CurrentIdentity.ts
+++ src/CurrentIdentity.ts
@@ -3,8 +3,6 @@ import {
   Context,
   createSecurityToken,
   CreateSecurityTokenWithTickerParams,
-  createVenue,
-  CreateVenueParams,
   inviteAccount,
   InviteAccountParams,
   modifySignerPermissions,
@@ -16,7 +14,6 @@ import {
   SecurityToken,
   TickerReservation,
   toggleFreezeSecondaryKeys,
-  Venue,
 } from '~/internal';
 import { NoArgsProcedureMethod, PermissionType, ProcedureMethod, Signer } from '~/types';
 import { createProcedureMethod } from '~/utils/internal';
@@ -71,10 +68,6 @@ export class CurrentIdentity {
       { getProcedureAndArgs: args => [inviteAccount, { ...args }] },
       context
     );
-    this.createVenue = createProcedureMethod(
-      { getProcedureAndArgs: args => [createVenue, args] },
-      context
-    );
     this.freezeSecondaryKeys = createProcedureMethod(
       { getProcedureAndArgs: () => [toggleFreezeSecondaryKeys, { freeze: true }], voidArgs: true },
       context
@@ -120,11 +113,6 @@ export class CurrentIdentity {
    *   fetch its pending Authorization Requests by calling `authorizations.getReceived`
    */
   public inviteAccount: ProcedureMethod<InviteAccountParams, AuthorizationRequest>;
-
-  /**
-   * Create a Venue under the ownership of the Current Identity
-   */
-  public createVenue: ProcedureMethod<CreateVenueParams, Venue>;
 
   /**
    * Freeze all the secondary keys in the Current Identity. This means revoking their permission to perform any operation on the blockchain and freezing their funds until the keys are unfrozen via [[unfreezeSecondaryKeys]]
