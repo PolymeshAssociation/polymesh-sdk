a713ebd142be03c6015717f6a37425f5b305935f
Jeremias Diaz <monitz87@gmail.com>
2022-02-25 16:31:11 -0300

    feat: 🎸 remove LocalSigningManager code

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import BigNumber from 'bignumber.js';
 import './polkadot/augment-api';
 
 export { Polymesh } from './Polymesh';
-export { LocalSigningManager } from './LocalSigningManager';
 export { Keyring, WsProvider } from '@polkadot/api';
 export { BigNumber };
