a98c19e4a76220de5c69ab7e2916b306086897ce
polymath-eric <86971143+polymath-eric@users.noreply.github.com>
2022-08-11 10:30:41 -0400

    fix: 🐛 update polymathnetwork to association references (#817)

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  repositoryUrl: 'https://github.com/PolymathNetwork/polymesh-sdk.git',
+  repositoryUrl: 'https://github.com/PolymeshAssociation/polymesh-sdk.git',
   branches: [
     'master',
     {
