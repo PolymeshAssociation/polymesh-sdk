49930d63f07d0256b68b45225a8474a517869223
Eric <ericrichardson@polymath.network>
2023-01-12 14:05:56 -0500

    feat: 🎸 support chain node v5.1.3

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -109,7 +109,7 @@ export const ROOT_TYPES = rootTypes;
 /**
  * The Polymesh RPC node version range that is compatible with this version of the SDK
  */
-export const SUPPORTED_NODE_VERSION_RANGE = '5.1.1';
+export const SUPPORTED_NODE_VERSION_RANGE = '5.1.3';
 
 /**
  * The Polymesh chain spec version range that is compatible with this version of the SDK
