88ac69c13297ab03006b8e8fa7c14fd452ccaf38
Prashant Bajpai <prashant.bajpai19@gmail.com>
2023-04-18 19:31:48 +0530

    feat: 🎸 Update middleware types compatible with 5.4.x chain

--- scripts/processMiddlewareTypes.js
+++ scripts/processMiddlewareTypes.js
@@ -7,15 +7,17 @@ const enumAliasFile = path.resolve('src', 'middleware', 'enumsV2.ts');
 
 let rawData = fs.readFileSync(typesV2File).toString('utf8');
 
-const enumRegex = new RegExp(/\/\*\* @enum\\n@enumName (.*) \*\/\nexport enum (.*) {/g);
+const enumRegex = new RegExp(
+  /\/\*\* @enum\\n@enumName (.*)Enum(?:\\n (?:.*))* \*\/\nexport enum (.*) {/g
+);
 
 const matches = rawData.matchAll(enumRegex);
 
 let aliases = [];
 let enumMappings = [];
 for (const match of matches) {
-  aliases.push(`${match[2]} as ${match[1]}`);
-  enumMappings.push(`${match[1]}: '${match[2]}'`);
+  aliases.push(`${match[2]} as ${match[1]}Enum`);
+  enumMappings.push(`${match[1]}Enum: '${match[2]}'`);
 }
 
 fs.writeFileSync(
