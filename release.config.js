2b24b51253344ea49ac41b2a6acc14625bfd8901
Prashant Bajpai <34747455+prashantasdeveloper@users.noreply.github.com>
2024-02-06 18:39:27 +0530

    feat: 🎸 Add release config for CA releases

--- release.config.js
+++ release.config.js
@@ -10,6 +10,10 @@ module.exports = {
       name: 'alpha',
       prerelease: true,
     },
+    {
+      name: 'confidential-assets',
+      prerelease: true,
+    },
   ],
   /*
    * In this order the **prepare** step of @semantic-release/npm will run first
