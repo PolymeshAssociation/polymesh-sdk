990d8d1167f2b02444702ea61cc6795ab29136bc
Prashant Bajpai <34747455+prashantasdeveloper@users.noreply.github.com>
2024-08-20 16:13:04 +0530

    test: 💍 update broken tests

--- src/api/entities/Portfolio/__tests__/index.ts
+++ src/api/entities/Portfolio/__tests__/index.ts
@@ -557,7 +557,9 @@ describe('Portfolio class', () => {
     const blockHash2 = 'otherHash';
 
     const assetId1 = '0x1111';
+    const ticker1 = 'TICKER_1';
     const assetId2 = '0x2222';
+    const ticker2 = 'TICKER_2';
 
     const amount1 = new BigNumber(1000);
     const amount2 = new BigNumber(2000);
@@ -582,10 +584,22 @@ describe('Portfolio class', () => {
     it('should return a list of transactions', async () => {
       let portfolio = new NonAbstract({ id, did }, context);
 
+      const getAssetIdFromMiddlewareSpy = jest.spyOn(
+        utilsInternalModule,
+        'getAssetIdFromMiddleware'
+      );
+      when(getAssetIdFromMiddlewareSpy)
+        .calledWith({ id: assetId1, ticker: ticker1 }, context)
+        .mockReturnValue(assetId1);
+      when(getAssetIdFromMiddlewareSpy)
+        .calledWith({ id: assetId2, ticker: ticker2 }, context)
+        .mockReturnValue(assetId2);
+
       const legs1 = [
         {
           legType: LegTypeEnum.Fungible,
           assetId: assetId1,
+          ticker: ticker1,
           amount: amount1,
           direction: SettlementDirectionEnum.Incoming,
           addresses: ['5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'],
@@ -599,6 +613,7 @@ describe('Portfolio class', () => {
         {
           legType: LegTypeEnum.Fungible,
           assetId: assetId2,
+          ticker: ticker2,
           amount: amount2,
           direction: SettlementDirectionEnum.Outgoing,
           addresses: ['5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'],
@@ -712,7 +727,10 @@ describe('Portfolio class', () => {
                     blockId: blockNumber1.toNumber(),
                     hash: 'someHash',
                   },
-                  assetId: assetId2,
+                  asset: {
+                    id: assetId2,
+                    ticker: ticker2,
+                  },
                   amount: amount2,
                   address: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
                   from: {
