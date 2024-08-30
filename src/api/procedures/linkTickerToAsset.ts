cc6d06e28150f85c2ed170a1d35ec547e138e44f
Eric Richardson <eric@polymesh.network>
2024-08-30 17:13:18 -0400

    feat: 🎸 auto register ticker when linking to asset
    batch a register ticker tx if needed when linking to an asset

--- src/api/procedures/linkTickerToAsset.ts
+++ src/api/procedures/linkTickerToAsset.ts
@@ -6,8 +6,14 @@ import {
   TickerReservationStatus,
   TxTags,
 } from '~/types';
-import { ExtrinsicParams, ProcedureAuthorization, TransactionSpec } from '~/types/internal';
+import {
+  BatchTransactionSpec,
+  ExtrinsicParams,
+  ProcedureAuthorization,
+  TransactionSpec,
+} from '~/types/internal';
 import { assetToMeshAssetId, stringToTicker } from '~/utils/conversion';
+import { checkTxType } from '~/utils/internal';
 
 /**
  * @hidden
@@ -16,20 +22,30 @@ export type Params = LinkTickerToAssetParams & {
   asset: BaseAsset;
 };
 
+export interface Storage {
+  status: TickerReservationStatus;
+}
+
 /**
  * @hidden
  */
 export async function prepareLinkTickerToAsset(
-  this: Procedure<Params, void>,
+  this: Procedure<Params, void, Storage>,
   args: Params
-): Promise<TransactionSpec<void, ExtrinsicParams<'asset', 'linkTickerToAssetId'>>> {
+): Promise<
+  | BatchTransactionSpec<void, unknown[][]>
+  | TransactionSpec<void, ExtrinsicParams<'asset', 'linkTickerToAssetId'>>
+> {
   const {
     context: {
       polymeshApi: { tx },
       isV6,
     },
+    storage: { status },
     context,
   } = this;
+
+  const { ticker, asset } = args;
 
   if (isV6) {
     throw new PolymeshError({
@@ -38,10 +54,32 @@ export async function prepareLinkTickerToAsset(
     });
   }
 
-  const { ticker, asset } = args;
+  if (status === TickerReservationStatus.AssetCreated) {
+    throw new PolymeshError({
+      code: ErrorCode.UnmetPrerequisite,
+      message: 'The ticker has already been linked to an asset',
+      data: { ticker },
+    });
+  }
 
   const rawTicker = stringToTicker(ticker, context);
   const rawAssetId = assetToMeshAssetId(asset, context);
+
+  if (status === TickerReservationStatus.Free) {
+    return {
+      transactions: [
+        checkTxType({
+          transaction: tx.asset.registerUniqueTicker,
+          args: [rawTicker],
+        }),
+        checkTxType({
+          transaction: tx.asset.linkTickerToAssetId,
+          args: [rawTicker, rawAssetId],
+        }),
+      ],
+      resolver: undefined,
+    };
+  }
 
   return {
     transaction: tx.asset.linkTickerToAssetId,
@@ -54,32 +92,54 @@ export async function prepareLinkTickerToAsset(
  * @hidden
  */
 export async function getAuthorization(
-  this: Procedure<Params, void>,
+  this: Procedure<Params, void, Storage>,
   { asset, ticker }: Params
 ): Promise<ProcedureAuthorization> {
-  const { context } = this;
-
-  const reservation = new TickerReservation({ ticker }, context);
-  const [{ status }] = await Promise.all([reservation.details(), context.getSigningIdentity()]);
-
   const transactions = [TxTags.asset.LinkTickerToAssetId];
+  const { status } = this.storage;
 
   if (status === TickerReservationStatus.Free) {
-    transactions.push(TxTags.asset.RegisterUniqueTicker); // not sure if register is needed
+    transactions.push(TxTags.asset.RegisterUniqueTicker);
   }
 
-  return {
+  const auth = {
     permissions: {
       assets: [asset],
-      transactions: [TxTags.asset.LinkTickerToAssetId],
+      transactions,
       portfolios: [],
     },
-    roles: [{ type: RoleType.TickerOwner, ticker }],
+  };
+
+  if (status === TickerReservationStatus.Reserved) {
+    return {
+      ...auth,
+      roles: [{ type: RoleType.TickerOwner, ticker }],
+    };
+  }
+
+  return auth;
+}
+
+/**
+ * @hidden
+ */
+export async function prepareStorage(
+  this: Procedure<Params, void, Storage>,
+  args: Params
+): Promise<Storage> {
+  const { context } = this;
+  const { ticker } = args;
+
+  const reservation = new TickerReservation({ ticker }, context);
+  const { status } = await reservation.details();
+
+  return {
+    status,
   };
 }
 
 /**
  * @hidden
  */
-export const linkTickerToAsset = (): Procedure<Params, void> =>
-  new Procedure(prepareLinkTickerToAsset, getAuthorization);
+export const linkTickerToAsset = (): Procedure<Params, void, Storage> =>
+  new Procedure(prepareLinkTickerToAsset, getAuthorization, prepareStorage);
