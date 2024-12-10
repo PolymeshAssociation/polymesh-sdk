96153f1618a2ce0b440b0071acf100f1fb26685c
Eric Richardson <eric@polymesh.network>
2024-12-10 17:55:49 -0500

    feat: 🎸 add getController method to account.staking
    allow account to get the designated controller account

--- src/api/entities/Account/Staking/index.ts
+++ src/api/entities/Account/Staking/index.ts
@@ -1,6 +1,7 @@
-import { bondPolyx, Context, Namespace } from '~/internal';
-import { Account, BondPolyxParams, ProcedureMethod } from '~/types';
-import { createProcedureMethod } from '~/utils/internal';
+import { Account, bondPolyx, Context, Namespace } from '~/internal';
+import { BondPolyxParams, ProcedureMethod } from '~/types';
+import { accountIdToString, stringToAccountId } from '~/utils/conversion';
+import { asAccount, createProcedureMethod } from '~/utils/internal';
 
 /**
  * Handles all Staking related functionality
@@ -27,4 +28,36 @@ export class Staking extends Namespace<Account> {
    * Bond POLYX for staking
    */
   public bond: ProcedureMethod<BondPolyxParams, void>;
+
+  /**
+   * Fetch the controller associated to this account if there is one
+   *
+   * @note if this is set it implies this account is a stash account
+   */
+  public async getController(): Promise<Account | null> {
+    const {
+      context,
+      context: {
+        polymeshApi: { query },
+      },
+    } = this;
+
+    const rawId = stringToAccountId(this.parent.address, context);
+
+    const rawController = await query.staking.bonded(rawId);
+
+    if (rawController.isNone) {
+      return null;
+    }
+
+    const unwrapped = rawController.unwrap();
+
+    console.log('unwrap', unwrapped.toString());
+
+    const address = accountIdToString(rawController.unwrap());
+
+    console.log('address: ', address);
+
+    return new Account({ address }, context);
+  }
 }
